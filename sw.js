ce9e89dc07ddcf9750c6f14877188b3289ff49aa
hemuyanling <2969545767@qq.com>
2024-11-19 17:40:15 +0800

    Site updated: 2024-11-19 17:40:15

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732009117111";
+const VERSION = "1732009207390";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
